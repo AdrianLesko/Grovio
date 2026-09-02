a89867c3cbfb5c97fa819e88e3d85110eb9abce5
Adrian Lesko <lesko.ado.adrian@gmail.com>
2026-09-02 20:49:18 +0200

    add main layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 
 import './App.css'
+import Header from './components/UI/Header'
+import Hero from './components/UI/Hero'
+import Footer from './components/UI/Footer'
 
 function App() {
 
-  return 
+  return (
+    <div className='flex flex-col h-screen'>
+        <Header />
+        <Hero />
+        <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/UI/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Header () {
+  return (
+    <div className='navbar bg-base-200'>
+      <h1 className='font-extrabold text-'>Grovio</h1>
+    </div>
+  )
+}
