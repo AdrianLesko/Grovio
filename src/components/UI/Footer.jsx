5fc28f4c0da5f77c5b54001c7192557d51495d71
Adrian Lesko <lesko.ado.adrian@gmail.com>
2026-09-04 10:54:37 +0200

    fix footer content justify to start

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer () {
   return (
-    <div className='footer fixed bottom-0 left-0 w-screen h-10 bg-base-100 flex justify-center items-center gap-1'>
+    <div className='footer fixed bottom-0 left-0 w-screen h-10 bg-base-100 flex justify-start items-center gap-1'>
       <img src='src/assets/user.png' className='w-3'/>
       <p className='font-bold'>Adrián Leško | 2026</p>
     </div>
